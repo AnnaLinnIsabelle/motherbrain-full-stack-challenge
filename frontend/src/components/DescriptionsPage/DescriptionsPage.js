b97d2809285d2572516f9503882446cfe6a1f3de
Linn Abrahamsson <linn.abrahamsson@doctrin.se>
2023-01-23 18:01:49 +0100

    remove selected orgs

--- frontend/src/components/DescriptionsPage/DescriptionsPage.js
+++ frontend/src/components/DescriptionsPage/DescriptionsPage.js
@@ -23,6 +23,27 @@ export const DescriptionPage = () => {
         });
     }
 
+    const isOrgSelected = (uuid) => {
+        return selectedOrgsByTerm[searchStr]?.orgs.find(org => org.uuid === uuid);
+    }
+
+    const removeSelectedOrg = (descriptionTerm, uuid) => {
+        const updatedOrgs = selectedOrgsByTerm[descriptionTerm].orgs.filter(org => org.uuid !== uuid);
+        if (!updatedOrgs.length) {
+            const selectedOrgsByTermCopy = {...selectedOrgsByTerm};
+            const updatedSelectedOrgsByTerm = delete selectedOrgsByTermCopy[descriptionTerm];
+            setSelectedOrgsByTerm(updatedSelectedOrgsByTerm);
+        } else {
+            setSelectedOrgsByTerm({
+                ...selectedOrgsByTerm,
+                [descriptionTerm]: {
+                    ...selectedOrgsByTerm[descriptionTerm],
+                    orgs: updatedOrgs,
+                }
+            });
+        }
+    }
+
     return (
         <div className='pageContainer'>
             <section className='scatterPlotSection'>
@@ -33,14 +54,14 @@ export const DescriptionPage = () => {
                 <input type='text' onChange={(e) => setSearchStr(e.target.value)}/>
                 <div className='resultCardContainer'>
                     {isLoading && <h3>...Searching</h3>}
-                    {!isLoading && results.map(org => <CompanyCard key={org.uuid} org={org} onOrgSelect={() => onOrgSelected(org)} />)}
+                    {!isLoading && results.map(org => <CompanyCard key={org.uuid} org={org} onOrgSelect={() => onOrgSelected(org)} isSelected={isOrgSelected(org.uuid)}/>)}
                 </div>
             </section>
             <section className='selectedSection'>
                 {Object.entries(selectedOrgsByTerm).map(([descriptionTerm, value]) => (
                     <div key={descriptionTerm} className='selectionBox' style={{borderColor: value.color}}>
                         <h4 style={{color: value.color}}>{descriptionTerm}</h4>
-                        {value.orgs.map(org => <div key={org.uuid} className='selectedOrg'><p>{org.company_name}</p><button className='removeButton'>X</button></div>)}
+                        {value.orgs.map(org => <div key={org.uuid} className='selectedOrg'><p>{org.company_name}</p><button className='removeButton' onClick={() => removeSelectedOrg(descriptionTerm, org.uuid)}>X</button></div>)}
                     </div>
                 ))}
             </section>
